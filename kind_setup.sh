d367de49aaa601e73d13f31d5f2e70f872979b9e
John Miller <john.e.miller@saic.com>
2025-05-21 14:38:17 -0400

    Updated setup script

--- kind_setup.sh
+++ kind_setup.sh
@@ -25,36 +25,109 @@ kind_setup() {
     echo "Creating cluster... ${CN}"
     echo "In region... ${REGION}"
 
-    cat <<EOF > kind-config.yaml
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-nodes:
-- role: control-plane
-  image: kindest/node:${CLUSTER_VERSION}
-  kubeadmConfigPatches:
-  labels:
-    topology.kubernetes.io/region: ${REGION}
-    topology.kubernetes.io/zone: ${REGION}-a
-- role: worker
-  image: kindest/node:${CLUSTER_VERSION}
-  kubeadmConfigPatches:
-  labels:
-    topology.kubernetes.io/region: ${REGION}
-    topology.kubernetes.io/zone: ${REGION}-a
-- role: worker
-  image: kindest/node:${CLUSTER_VERSION}
-  kubeadmConfigPatches:
-  labels:
-    topology.kubernetes.io/region: ${REGION}
-    topology.kubernetes.io/zone: ${REGION}-a
-EOF
-
+    cat <<-EOF > kind-config.yaml
+    kind: Cluster
+    apiVersion: kind.x-k8s.io/v1alpha4
+    nodes:
+    - role: control-plane
+      image: kindest/node:${CLUSTER_VERSION}
+      kubeadmConfigPatches:
+      labels:
+        topology.kubernetes.io/region: ${REGION}
+        topology.kubernetes.io/zone: ${REGION}-a
+    - role: worker
+      image: kindest/node:${CLUSTER_VERSION}
+      kubeadmConfigPatches:
+      labels:
+        topology.kubernetes.io/region: ${REGION}
+        topology.kubernetes.io/zone: ${REGION}-a
+    - role: worker
+      image: kindest/node:${CLUSTER_VERSION}
+      kubeadmConfigPatches:
+      labels:
+        topology.kubernetes.io/region: ${REGION}
+        topology.kubernetes.io/zone: ${REGION}-a
+    EOF
+    
     # Create cluster
     kind create cluster --name="${CN}" --config=kind-config.yaml
     
     # Clean up config file
     rm kind-config.yaml
+
+    # Install Cilium and Metallb
+ .  install_cilium "$CN"
+    install_metallb "$CN"
 }
 
-# Execute the setup function with provided arguments
-# kcc dymium v1.33.1 us-gov-west-1
+# Function to install cilium
+install_cilium() {
+    local CN=$1
+
+    # Install Cilium
+    helm repo add cilium https://helm.cilium.io/
+    helm repo update
+    VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/cilium/cilium/releases/latest)
+    CILIUM_VERSION=${VERSION##*/}
+    CILIUM_SHA_TAG="sha256:bfeb3f1034282444ae8c498dca94044df2b9c9c8e7ac678e0b43c849f0b31746"
+
+    docker pull quay.io/cilium/cilium:$CILIUM_VERSION
+    kind load docker-image quay.io/cilium/cilium:$CILIUM_VERSION
+
+    helm install cilium cilium/cilium --version $CILIUM_VERSION \
+      --namespace kube-system \
+      --set image.pullPolicy=IfNotPresent \
+      --set ipam.mode=kubernetes
+
+    FILE=~/bin/cilium
+    if [ ! -f "$FILE" ]; then
+      VERSION=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/cilium/cilium-cli/releases/latest)
+      CILIUM_CLI_VERSION=${VERSION##*/}
+      CLI_ARCH=amd64
+      if [ "$(uname -m)" = "arm64" ]; then CLI_ARCH=arm64; fi
+      curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-darwin-${CLI_ARCH}.tar.gz{,.sha256sum}
+      shasum -a 256 -c cilium-darwin-${CLI_ARCH}.tar.gz.sha256sum
+      tar xzvfC cilium-darwin-${CLI_ARCH}.tar.gz ~/bin
+      rm cilium-darwin-${CLI_ARCH}.tar.gz{,.sha256sum}
+    fi
+
+    echo "Check cilium status"
+    cilium status --wait --context ${CN}
+}
+
+# Function to install metallb
+install_metallb() {
+    local CN=$1
+    
+    # Inspect the Docker network
+    OCTET=$(docker network inspect -f '{{.IPAM.Config}}' kind | sed 's/.*[0-9][0-9][0-9]\\.\\([0-9][0-9]\\)\\..*/\\1/')
+
+    # Install Metallb
+    METALLB_VERSION="0.14.3"
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v$METALLB_VERSION/config/manifests/metallb-native.yaml --context ${CN}
+    kubectl --context ${CN} wait --for=condition=Ready  pod -l app=metallb -n metallb-system --timeout=120s
+    kubectl apply --context ${CN} -f -<<-EOF
+    apiVersion: metallb.io/v1beta1
+    kind: IPAddressPool
+    metadata:
+      name: ip-pool
+      namespace: metallb-system
+    spec:
+      addresses:
+      - 172.${OCTET}.255.200-172.${OCTET}.255.250
+    ---
+    apiVersion: metallb.io/v1beta1
+    kind: L2Advertisement
+    metadata:
+      name: empty
+      namespace: metallb-system
+    spec:
+      ipAddressPools:
+      - ip-pool
+    EOF
+}
+
+# Main execution
+kcc dymium v1.33.1 us-gov-west-1
+
+
